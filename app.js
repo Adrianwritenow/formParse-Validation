b4d417b09117402a7c05e5edff7e2d957180ed58
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-07-26 11:25:51 -0400

    Added  form and collect data from user, returns in simple pg

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require ("express");
+const mustacheExpress = require("mustache-express");
+const bodyParser = require('body-parser');
 
 var app = express();
 
+//configure mustache with express
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+app.use(express.static('public'));
+
+//configure body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', function(request, response){
-  response.send("hello!?!? im alive!!!!!!!")
+  response.render('form');
+});
+app.post('/', function(request, response){
+response.render('answers', {answers: request.body });
 });
 app.listen(3000, function(){
   console.log('Server Farted');
